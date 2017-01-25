4281ec1bacc665bec1224aaeb0870936e75d3bd0
Jenny Goodchild <scaryashmorph@gmail.com>
2017-01-25 22:19:57 +1000

    Add player movement detection and synced footsteps on sand audio

--- Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
+++ Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
@@ -9,44 +9,129 @@ public class CompletePlayerController : MonoBehaviour {
 	public float speed;				//Floating point variable to store the player's movement speed.
 	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
 	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+    public AudioSource footsteps;  //Store a reference to the AudioSource attached to the player for footsteps
 
-	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
 	private int count;				//Integer to store the number of pickups collected so far.
+    private bool moving;            //Whether or not the player is currently moving
+    private bool startedMoving;     //Whether or not the player just started moving
+    private bool stoppedMoving;     //Whether or not the player just stopped moving
+    private bool movingLeft;        //Whether or not the player is currently moving left
+    private bool movingRight;       //Whether or not the player is currently moving right
+    private bool movingUp;          //Whether or not the player is currently moving up
+    private bool movingDown;        //Whether or not the player is currently moving down
 
-	// Use this for initialization
-	void Start()
+
+    // initialization
+    void Start()
 	{
-		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		//store reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
 
-		//Initialize count to zero.
-		count = 0;
+        //Initialize item pickup count to zero.
+        count = 0;
 
-		//Initialze winText to a blank string since we haven't won yet at beginning.
+		//Initialze winText to a blank string at beginning.
 		winText.text = "";
 
 		//Call our SetCountText function which will update the text with the current value for count.
 		SetCountText ();
 	}
 
-	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	//FixedUpdate called at a fixed interval and is independent of frame rate. Put physics code here.
 	void FixedUpdate()
 	{
-		//Store the current horizontal input in the float moveHorizontal.
+		//Store the current horizontal and vertical inputs
 		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+        float moveVertical = Input.GetAxisRaw ("Vertical");
 
-		//Store the current vertical input in the float moveVertical.
-		float moveVertical = Input.GetAxisRaw ("Vertical");
+        // Check and set movement indicator variables, and call player movement functions
+        checkAndHandleMovement(moveHorizontal, moveVertical);
 
-		//Use the two store floats to create a new Vector2 variable movement.
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
 		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
 		rb2d.AddForce (movement * speed);
-	}
+    }
 
-	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
-	void OnTriggerEnter2D(Collider2D other) 
+    // Check and set movement indicators based on horizontal and vertical axis inputs
+    private void checkAndHandleMovement(float moveHorizontal, float moveVertical)
+    {
+        // Reset all indicators prior to checking (except for previous overall player moving indicator)
+        movingLeft = false;
+        movingRight = false;
+        movingUp = false;
+        movingDown = false;
+        startedMoving = false;
+        stoppedMoving = false;
+
+        if ((moveHorizontal == 0) && (moveVertical == 0)) // There is no movement
+        {
+            if (moving == true) // just stopped moving
+            {
+                stoppedMoving = true;
+                playerStoppedMoving();
+            }
+            moving = false;   
+        }
+        else // There is movement. TODO: set variables for diagonal movement ready for animation handling
+        {
+            if (moving == false) // just started moving
+            {
+                startedMoving = true;
+                playerStartedMoving();
+            }
+            moving = true;
+            if (moveHorizontal < 0) // moving left
+            {
+                print("Moving left");
+                movingLeft = true;
+            }
+            if (moveHorizontal > 0) // moving right
+            {
+                print("Moving right");
+                movingRight = true;
+            }
+            if (moveVertical > 0) // moving up
+            {
+                movingUp = true;
+                print("Moving up");
+            }
+            if (moveVertical < 0) // moving down
+            {
+                movingDown = true;
+                print("Moving down");
+            }
+        }
+    }
+
+    // This method will be called whenever player starts moving
+    private void playerStartedMoving()
+    {
+        // We will start playing the footsteps IF they are not already playing
+        footsteps.Play();  
+    }
+
+    // This method will be called whenever player is moving
+    private void playerIsMoving()
+    {
+        // Any thing that needs to happen all the time while player is moving e.g. animation
+    }
+
+    // This method will be called whenever player stops moving
+    private void playerStoppedMoving()
+    {
+        footsteps.Stop();
+    }
+
+    private bool anyMovementKeyDepressed()
+    {
+        return (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D));
+    }
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other) 
 	{
 		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
 		if (other.gameObject.CompareTag ("PickUp")) 
@@ -61,7 +146,6 @@ public class CompletePlayerController : MonoBehaviour {
 			SetCountText ();
 		}
 		
-
 	}
 
 	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
