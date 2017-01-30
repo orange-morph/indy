dc1a058bbf474d85e912199cd32351fa68c711ec
Jenny Goodchild <scaryashmorph@gmail.com>
2017-01-30 19:24:58 +1000

    Inventory development & item stacking.

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -33,26 +33,61 @@ public class InventoryController : MonoBehaviour {
         AddItem(4);
         AddItem(0);
         AddItem(3);
+        AddItem(4);
         AddItem(2);
+        AddItem(4);
+        AddItem(4);
+        AddItem(4);
+        AddItem(4);
     }
 
     // Fetches item by id from database; loops through inventory items until it finds one with dummy item, replaces it.
     public void AddItem(int id)
     {
         Item itemToAdd = database.FetchItemByID(id);
-        for (int i = 0; i < items.Count; i++)
+
+        if (itemToAdd.Stackable && CheckItemInInventory(itemToAdd) ) // if the item is already present in the inventory and is stackable
         {
-            if (items[i].ID == -1) // If the inventory slot has a dummy item in it, replace with our real item.
+            // find the inventory item again and increment its stackable amount counter by 1
+            for (int i = 0; i < items.Count; i++) // loop through items already held in inventory
             {
-                items[i] = itemToAdd;
-                GameObject itemObject = Instantiate(inventoryItem);
-                itemObject.transform.SetParent(slots[i].transform);
-                itemObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
-                //itemObject.transform.position = new Vector2(0,0);
-                break;
+                if (items[i].ID == id) // matched in inventory
+                {
+                    ItemData data = slots[i].transform.GetChild(0).GetComponent<ItemData>();
+                    data.amount++; // At the moment, all we do is increment by 1. In future, we will have stack-splitting etc, so this will change.
+                    data.transform.GetChild(0).GetComponent<Text>().text = data.amount.ToString();
+                    break;
+                }
+            }
+        } 
+        else // otherwise, it does not stack, so we'll just add it as normal.
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].ID == -1) // If the inventory slot has a dummy item in it, replace with our real item.
+                {
+                    items[i] = itemToAdd;
+                    GameObject itemObject = Instantiate(inventoryItem);
+                    itemObject.transform.SetParent(slots[i].transform);
+                    itemObject.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                    //itemObject.transform.position = new Vector2(0,0);
+                    break;
+                }
             }
         }
 
+    }
+
+    public bool CheckItemInInventory(Item item)
+    {
+        for (int i = 0; i < items.Count; i++) // loop through items already held in inventory
+        {
+            if (items[i].ID == item.ID) // matched in inventory
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour {
+
+    public Item item; // 
+    public int maxAmount; // will (later) hold the max stackable amount for this item..
+    public int amount; // how much of this item do we have in our inventory (stack size)
+
+
+
+
+
+
+
+
+}
