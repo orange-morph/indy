cc3c53e905bd7b60f013bc265e91c0b911f41bdc
Jenny Goodchild <scaryashmorph@gmail.com>
2017-02-11 19:44:12 +1000

    Set x/y camera tracking bounds against player via camera controller script

--- Assets/Scripts/CompleteCameraController.cs
+++ Assets/Scripts/CompleteCameraController.cs
@@ -13,10 +13,6 @@ public class CompleteCameraController : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-        // Load SpriteTile map level
-        //Tile.SetCamera();
-        //Tile.LoadLevel(level);
-
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
 
@@ -27,5 +23,23 @@ public class CompleteCameraController : MonoBehaviour {
 	{
 		// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
 		transform.position = player.transform.position + offset;
-	}
+        var pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        // Control the bounds of the camera tracking on the player by x and y
+        if (pos.x < 20) {
+            pos.x = Mathf.Clamp(pos.x, 20, 20);
+        }
+        if (pos.y < 10)
+        {
+            pos.y = Mathf.Clamp(pos.y, 10, 10);
+        }
+        if (pos.x > 80)
+        {
+            pos.x = Mathf.Clamp(pos.x, 80, 80);
+        }
+        if (pos.y > 90)
+        {
+            pos.y = Mathf.Clamp(pos.y, 90, 90);
+        }
+        transform.position = pos;
+    }
 }
