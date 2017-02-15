51f228fbde5775c6dd627d46eb30163a23379710
Jenny Goodchild <scaryashmorph@gmail.com>
2017-02-15 13:23:40 +1000

    Updated quest and dialogue, added window toggler script

---
+++ Assets/Scripts/WindowToggler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class WindowToggler : MonoBehaviour
+{
+
+    public UnityUIQuestLogWindow questLogWindow; // Assign in inspector
+    private bool questLogIsShowing;
+
+    // Use this for initialization
+    void Start()
+    {
+        questLogIsShowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            questLogIsShowing = !questLogIsShowing;
+            if (questLogIsShowing)
+            {
+                questLogWindow.Open();
+            }
+            else
+            {
+                questLogWindow.Close();
+            }
+        }
+
+    }
+}
